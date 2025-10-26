fa64b72971fdb4f794c00d4b74dae4cb994c4ba3
F9NNEC <adityatriandriyansyah9@gmail.com>
2025-10-26 21:29:44 +0700

    Route wild card, passing data to view

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -4,11 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>About Us</h1>
-    <p>Hello There! My name is {{ $nama }}</p>
-    <p>I'm {{ $umur }} years old</p>
+    <h1>Dashboard Page</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,38 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('Home');
+    return view('home');
 });
 
-Route::view('/about', 'about');
+Route::get('/about', function () {
+    $nama = 'F9NNEC';
+    $umur = 18;
+    return view('about', ['nama'=> $nama, 'umur'=> $umur]);
+    // return view('about', compact('nama','umur'));
+    // return view('about')
+    //     ->with('nama', 'F9NNEC')
+    //     ->with('umur', 19);
+});
+
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/user', function () {
+    return view('user.dashboard');
+});
+
+Route::get('/user/{id}', function ($id) {
+    return view('user.profile', ['id'=> $id]);
+});
+
+// Route::get('/clear.cache', function () {
+//     $run = Artisan::call('config:clear');
+//     $run = Artisan::call('cache:clear');
+//     $run = Artisan::call('config:cache');
+//     return 'Finished';
+// });
