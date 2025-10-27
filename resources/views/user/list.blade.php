8bb383697b9a8a233d07247aae65f7bfc5a86314
F9NNEC <adityatriandriyansyah9@gmail.com>
2025-10-27 17:06:49 +0700

    Tailwind CSS

--- resources/views/user/list.blade.php
+++ resources/views/user/list.blade.php
@@ -1,22 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>User List</title>
-</head>
-<body>
+<x-layout>
     <h1>List User</h2>
-    <ul>
-        @foreach ($data as $item)
-            <li>
-                <p>Nama {{ $item['nama']}}, Role: {{ $item['role']}}, id: {{ $item['id']}}</p>
-            </li>
+        <div class="overflow-x-auto">
+            <table class="min-w-full divide-y-2 divide-gray-200">
+                <thead class="ltr:text-left rtl:text-right">
+                    <tr class="*:font-medium *:text-gray-900">
+                        <th class="px-3 py-2 whitespace-nowrap">Nama</th>
+                        <th class="px-3 py-2 whitespace-nowrap">Role</th>
+                        <th class="px-3 py-2 whitespace-nowrap">Id</th>
+                <th class="px-3 py-2 whitespace-nowrap">Tanggal</th>
+            </tr>
+        </thead>
+        
+        <tbody class="divide-y divide-gray-200">
+            @foreach ($data as $item)
+            <tr class="*:text-gray-900 *:first:font-medium">
+                <td class="px-3 py-2 whitespace-nowrap">{{ $item['nama'] }}</td>
+                <td class="px-3 py-2 whitespace-nowrap">{{ $item['role'] }}</td>
+                <td class="px-3 py-2 whitespace-nowrap">{{ $item['id'] }}</td>
+                <td class="px-3 py-2 whitespace-nowrap">04/06/1262</td>
+            </tr>
             @endforeach
+        </tbody>
+    </table>
         @if ($item['role'] == 'user')
             <p>No admin</p>
         @endif
-        </ul>
-    </body>
-</html>
+</div>    
+
+        <x-slot:footer>
+            <strong>User List</strong>
+        </x-slot:footer>
+</x-layout>
