111655e97a40b5f5d826d4f0566b6020c8dc358d
F9NNEC <adityatriandriyansyah9@gmail.com>
2025-10-28 16:04:34 +0700

    Active link

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -44,7 +44,7 @@
 
         <div class="flex items-center gap-4">
             <div class="sm:flex sm:gap-4">
-            <a class="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700" href="#">
+            <a class="block rounded-md bg-teal-600  px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700" href="#">
                 Login
             </a>
 
