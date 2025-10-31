5b093bf3707b017f168877dd80f344b42fb6ca0f
F9NNEC <adityatriandriyansyah9@gmail.com>
2025-10-31 14:54:16 +0700

    Factory, data dummy

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Siswa extends Model
 {
+    use HasFactory; 
     //
     protected $table = "siswa";
-    protected $fillable = ['nama', 'tanggal_lahir', 'role']; 
+    protected $fillable = ['nama', 'tanggal_lahir', 'role'];
 }
