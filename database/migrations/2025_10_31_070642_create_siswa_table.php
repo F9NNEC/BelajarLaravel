934b4e8c95ca86fc3325ae813d66d8d78f47d14a
F9NNEC <adityatriandriyansyah9@gmail.com>
2025-10-31 14:31:59 +0700

    Model, eloquent orm

--- database/migrations/2025_10_31_070642_create_siswa_table.php
+++ database/migrations/2025_10_31_070642_create_siswa_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user', function (Blueprint $table) {
+        Schema::create('siswa', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
             $table->date('tanggal_lahir');
             $table->string('role');
             $table->timestamps();
-            
         });
     }
 
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user');
+        Schema::dropIfExists('siswa');
     }
 };
